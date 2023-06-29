43d2ec4d686191f22fe68945074cd7bf6694f4c8
MrwanAmr185 <mrwanamr185@gmail.com>
2023-06-28 22:22:42 -0700

    a

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - _puts
+ *
+ * @str: the string input
+ *
+ * Return: non
+*/
+
+void _puts(char *str)
+{
+        while (*str != '\0')
+        {
+                _putchar(*str + 0);
+                str++;
+        }
+        _putchar('\n');
+}
